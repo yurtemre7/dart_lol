83f72e5ba6879a234a9dd1ab4ecf6b93460eebf1
yurtemre7 <emre.yurtseven.1@campus.tu-berlin.de>
2022-01-24 23:01:08 +0100

    improve test

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,28 +1,23 @@
 import 'package:dart_lol/dart_lol.dart';
 import 'package:dart_lol/key.dart';
 
-var emre = 'buff yi rep mid';
+var emre = 'teemo ist op';
 
 main() async {
   final league = League(apiToken: key, server: 'euw1');
   // print(league.server);
   var summonerInfo = await league.getSummonerInfo(summonerName: emre);
 
-  print(summonerInfo.lastTimeOnline);
-  assert(summonerInfo.lastTimeOnline! > 0);
-
+  DateTime lastOnline = summonerInfo.lastTimeOnline!;
+  print(lastOnline);
   print(summonerInfo.summonerName);
-  // Outputs buff yi rep mid
+  // Outputs "teemo ist op"
   print(summonerInfo.level);
   // Outputs current summoner level
 
   // etc.
-  var games = await league.getGameHistory(
-      accountID: summonerInfo.accID, summonerName: summonerInfo.summonerName);
+  var games =
+      await league.getGameHistory2(puuid: summonerInfo.puuid!, summonerName: summonerInfo.summonerName!);
   var game1Stats = await games![0].stats();
-  print(game1Stats.participants![game1Stats.playerIDinGame! - 1].championName);
-
-  var game2Stats = await games[1].stats();
-  print(game2Stats.participants![game2Stats.playerIDinGame! - 1].championName);
-  assert(game2Stats.participants![game2Stats.playerIDinGame! - 1].championName!.isNotEmpty);
+  // print(game1Stats);
 }
